fff8578f2fa613476dd5888d33aa735224a24272
Ayesha Batool <160726633+batoolayesha@users.noreply.github.com>
2024-03-24 14:19:44 +0500

    Adding number guessing game

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Printing a Welcome Message
+console.log("\n\Welcome To \'CodeWithAyesha\' - CLI Simple Calculator\n");
+//Asking Questions from users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter SecondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perfome Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+//Conditional Statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
